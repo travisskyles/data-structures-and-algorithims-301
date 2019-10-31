8af360f25f9d68faae1ed45bb5acaa980d11b3d3
tskyles <travis.skyles@gmail.com>
2019-10-30 20:13:29 -0700

    challenge 6 complete

--- code-challenges/sort/challenge-03.test.js
+++ code-challenges/sort/challenge-03.test.js
@@ -65,9 +65,7 @@ Write a function named sortNumbersByLength that takes in an array of numbers and
 For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
-const sortNumbersByLength = (arr) => {
-  // Solution code here...
-};
+const sortNumbersByLength = (arr) => arr.sort((a, b) => a.toString().length - b.toString().length);
 
 /*-----------------------------------------------------------------------------------------------
 CHALLENGE 7
