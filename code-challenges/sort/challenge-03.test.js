7fc4752465a9ffe69a4be460cecc8d29cc818549
tskyles <travis.skyles@gmail.com>
2019-10-30 19:52:38 -0700

    challenge 4 complete

--- code-challenges/sort/challenge-03.test.js
+++ code-challenges/sort/challenge-03.test.js
@@ -36,10 +36,7 @@ Write a function named alphabetizeBetter that takes in an array of strings and r
 For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, and so is ['alphabet', 'Alphabet', 'carrot', 'Zebra'].
 ------------------------------------------------------------------------------------------------ */
 
-const alphabetizeBetter = (arr) => {
-  // Solution code here...
-};
-
+const alphabetizeBetter = (arr) => arr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
