457be3282a6b05189a37d8b25cef4d847298cda6
tskyles <travis.skyles@gmail.com>
2019-10-30 21:43:12 -0700

    challenge 8 complete

--- code-challenges/sort/challenge-03.test.js
+++ code-challenges/sort/challenge-03.test.js
@@ -97,7 +97,15 @@ If two people have the same full name, the younger one should come first. Do not
 ------------------------------------------------------------------------------------------------ */
 
 const sortPeopleBetter = (arr) => {
-  // Solution code here...
+  return arr.sort((a, b) => {
+    if(a.lastName === b.lastName){
+      if(a.firstName === b.firstName){
+        return a.age - b.age;
+      }
+      return a.firstName.localeCompare(b.firstName);
+    }
+    return a.lastName.localeCompare(b.lastName);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
