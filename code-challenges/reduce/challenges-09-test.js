18a7d730cc7989d3272b93ca2942254d2abfffcf
travis skyles <travis.skyles@gmail.com>
2019-11-08 12:39:26 -0800

    challenge 3

--- code-challenges/reduce/challenges-09-test.js
+++ code-challenges/reduce/challenges-09-test.js
@@ -82,9 +82,7 @@ Write a function named reversedString that takes in a string and returns a strin
 Note: You must use reduce for this challenge. You may not use the built-in .reverse() string method.
 ------------------------------------------------------------------------------------------------ */
 
-const reversedString = (str) => {
-  // Solution code here...
-};
+const reversedString = (str) => str.split('').reduce((acc,curr) => curr + acc);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
