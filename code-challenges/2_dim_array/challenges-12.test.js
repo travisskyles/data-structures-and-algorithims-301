0dda5382898ac3cc7dc1f69f2a3b1b183c85f35e
travis skyles <travis.skyles@gmail.com>
2019-11-12 21:41:23 -0800

    challenge 3 done

--- code-challenges/2_dim_array/challenges-12.test.js
+++ code-challenges/2_dim_array/challenges-12.test.js
@@ -75,7 +75,13 @@ const errands = [
 ];
 
 const howManyTreats = (arr) => {
-  // Solution code here...
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i].items.length; j++){
+      if(arr[i].items[j].name === 'Treats'){
+        return arr[i].items[j].quantity;
+      }
+    }
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
