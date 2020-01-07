cd1281126598f33cc1d29721239735f8fcdfd6ee
travis skyles <travis.skyles@gmail.com>
2020-01-07 14:04:54 -0800

    test 1 done

--- 401/code-challenges/02-arrayShift/__tests__/arrayShift.test.js
+++ 401/code-challenges/02-arrayShift/__tests__/arrayShift.test.js
@@ -1,3 +1,12 @@
 'use strict';
 
-const insertShiftArray = require(insertShiftArray);
+const insertShiftArray = require('../arrayShift');
+
+//make sure inputs are the correct type
+//ensure value is placed in the correct spot
+
+describe('Make sure that the insertArrayShift function works correctly', () => {
+  it('returns an array with the new value in the middle', () => {
+    expect(insertShiftArray([1, 2, 3, 4], 5)).toStrictEqual([1, 2, 5, 3, 4]);
+  });
+});
